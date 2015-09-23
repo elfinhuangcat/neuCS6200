b286c6a97091bfc358260af819e5ad177c3dc2c5
Yaxin <elfinhuangcat@gmail.com>
2015-09-23 17:48:02 -0400

    updated the crawler and parser

--- CS6200/htmlparser.py
+++ CS6200/htmlparser.py
@@ -14,29 +14,33 @@ class MyHTMLParser(HTMLParser):
         if not self.key:
             self.valid = True    # Key phrase is not required. Always valid page.
     def handle_starttag(self, tag, attrs):
-        if tag == "a" and len(attrs) > 0 and self.valid:
+        if tag == "a" and len(attrs) > 0:
             attrd = dict(attrs)
             if "href" in attrd.keys():
                 # attrd["href"] is the link
-                if ("#" not in attrd["href"] and
-                    ":" not in attrd["href"] and 
-                    "&action=edit" not in attrd["href"] and 
-                    "en.wikipedia.org/wiki/Main_Page" not in attrd["href"]):
-                    # See if it is a page in WIKI:
-                    if (attrd["href"].startswith("/") and
-                        not attrd["href"].startswith("//")):
+                if (attrd["href"].startswith("http://en.wikipedia.org/wiki/") or
+                    attrd["href"].startswith("https://en.wikipedia.org/wiki/")):
+                    # Absolute en.wikipedia.org links
+                    if ('#' not in attrd["href"] and
+                        ':' not in attrd["href"][5:] and # the only difference
+                        "&action=edit" not in attrd["href"] and
+                        not (attrd["href"].endswith("wiki/Main_Page") or
+                             attrd["href"].endswith("wiki/Main_Page/"))):
+                        self.links.append(attrd["href"])
+                elif attrd["href"].startswith("/wiki"):
+                    # Relative wiki links
+                    if ('#' not in attrd["href"] and
+                        ':' not in attrd["href"] and # the only diffrence
+                        "&action=edit" not in attrd["href"] and
+                        not (attrd["href"].endswith("wiki/Main_Page") or
+                             attrd["href"].endswith("wiki/Main_Page/"))):
                         self.links.append("https://en.wikipedia.org" + 
                                           attrd["href"])
-                    elif (attrd["href"].startswith("http://en.wikipedia.org") or
-                          attrd["href"].startswith("https://en.wikipedia.org")):
-                        self.links.append(attrd["href"])
-                    elif ("en.wikipedia.org" in attrd["href"]):
-                        self.links.append(attrd["href"])
         elif tag == "body":
-            self.body = True
+            self.body = True # Flag begin body content
     def handle_endtag(self, tag):
         if tag == "body" and self.body:
-            self.body = False
+            self.body = False # Flag end body content
     def handle_data(self, data):
         # Handle the data with the body tag
         if self.body and self.key in data:
