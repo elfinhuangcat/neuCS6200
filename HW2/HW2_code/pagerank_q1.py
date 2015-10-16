1af549bf1055011ae8f5ae339de4cc3fe3f8cb32
Yaxin <elfinhuangcat@gmail.com>
2015-10-15 20:44:03 -0400

    q1 completed

--- HW2/HW2_code/pagerank_q1.py
+++ HW2/HW2_code/pagerank_q1.py
@@ -1,36 +1,61 @@
 #! /usr/bin/python2
-from graph import *
+
+from graph_mod import *
 import copy
 
-D = 0.85 # damping factor
-def page_rank(filepath):
-	graph = Graph(filepath)
-	# initial page rank vector:
-	pr_dict = dict.fromkeys(graph.get_in_graph().keys())
-	for key in pr_dict.keys():
-		pr_dict[key] = 1 / float(graph.get_node_num())
-	new_pr_dict = copy.copy(pr_dict)
-	i = 0
-    while i < 10:
-		i += 1
-		sink_pr = 0
-		for page in graph.get_sink_nodes():
-			sink_pr += pr_dict[key]
-		for p in graph.get_in_graph().keys():
-			new_pr_dict[p] = (1 - D)/graph.get_node_num()
-			new_pr_dict[p] += D * sink_pr / graph.get_node_num()
-			if len(graph.get_out_graph()[q]) > 0:
-				for q in graph.get_in_graph()[p]:
-					new_pr_dict[p] += D * pr_dict[q] / len(graph.get_out_graph()[q]
-		for page in graph.get_in_graph().keys():
-			pr_dict[page] = new_pr_dict[page]
-	return pr_dict
-				
-def print_pagerank_vector(pr_dict):
-	print("Page rank vector:")
-	for page in pr_dict.keys():
-		print(str(page) + ": " + str(pr_dict[page]))
-	print("Sum of page rank values: " + sum(pr_dict.values()))
+D = 0.85
+OUTPUT_DIR = "outputs/"
 
-if __name__ == '__main__':
-	page_rank("graph")
+
+def page_rank(filepath, iteration):
+    graph = Graph(filepath)
+    #initial page rank vector:
+    pr_dict = dict.fromkeys(graph.get_in_graph().keys())
+    for key in pr_dict.keys():
+        pr_dict[key] = 1 / float(graph.get_node_num())
+    new_pr_dict = copy.copy(pr_dict)
+    i = 0
+    
+    while i < iteration:
+        i += 1
+        sink_pr = 0
+        for page in graph.get_sink_nodes():
+            sink_pr += pr_dict[page]
+        for p in graph.get_in_graph().keys():
+            new_pr_dict[p] = (1 - D)/graph.get_node_num()
+            new_pr_dict[p] += D * sink_pr / graph.get_node_num()
+            for q in graph.get_in_graph()[p]:
+                if len(graph.get_out_graph()[q]) > 0:
+                    new_pr_dict[p] += D * pr_dict[q] / len(graph.get_out_graph()[q])
+                    
+        for page in graph.get_in_graph().keys():
+            pr_dict[page] = new_pr_dict[page]
+    return pr_dict
+    
+def print_pagerank_vector(pr_dict):
+    print("Page rank vector:")
+    for page in pr_dict.keys():
+        print(str(page) + ": " + str(pr_dict[page]))
+    print("Sum of page rank values: " + str(sum(pr_dict.values())))
+    
+def output_pagerank_vector(pr_dict, filename):
+    log = open((OUTPUT_DIR + filename), 'w')
+    for key in pr_dict.keys():
+        log.write(key + " " + str(pr_dict[key]) + "\n")
+    log.close()
+
+if __name__ == "__main__":
+    print("1 iteration:")
+    pr_dict = page_rank("graphs/six_node_graph", 1)
+    print_pagerank_vector(pr_dict)
+    output_pagerank_vector(pr_dict, "q1_1_iteration")
+    
+    print("\n10 iterations:")
+    pr_dict = page_rank("graphs/six_node_graph", 10)
+    print_pagerank_vector(pr_dict)
+    output_pagerank_vector(pr_dict, "q1_10_iteration")
+    
+    print("\n100 iterations:")
+    pr_dict = page_rank("graphs/six_node_graph", 100)
+    print_pagerank_vector(pr_dict)
+    output_pagerank_vector(pr_dict, "q1_100_iteration")
---
+++ HW2/HW2_code/pagerank_q1.py~
@@ -0,0 +1,37 @@
+#! /usr/bin/python2
+from graph_mod import *
+import copy
+D = 0.85 # damping factor
+def page_rank(filepath):
+    graph = Graph(filepath)
+    # initial page rank vector:
+    pr_dict = dict.fromkeys(graph.get_in_graph().keys())
+    for key in pr_dict:
+        pr_dict[key] = 1 / float(graph.get_node_num())
+    new_pr_dict = copy.copy(pr_dict)
+    i = 0
+
+    while i < 10:
+        i += 1
+        sink_pr = 0
+        for page in graph.get_sink_nodes():
+            sink_pr += pr_dict[key]
+        for p in graph.get_in_graph().keys():
+            new_pr_dict[p] = (1 - D)/graph.get_node_num()
+            new_pr_dict[p] += D * sink_pr / graph.get_node_num()
+            if len(graph.get_out_graph()[q]) > 0:
+                for q in (graph.get_in_graph())[p]:
+                    new_pr_dict[p] += (D * pr_dict[q] 
+                                       / len(graph.get_out_graph()[q])
+        for page in graph.get_in_graph().keys():
+            pr_dict[page] = new_pr_dict[page]
+    return pr_dict
+
+def print_pagerank_vector(pr_dict):
+    print("Page rank vector:")
+    for page in pr_dict.keys():
+        print(str(page) + ": " + str(pr_dict[page]))
+    print("Sum of page rank values: " + sum(pr_dict.values()))
+
+if __name__ == '__main__':
+    page_rank("graph")
