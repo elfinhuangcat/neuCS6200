27f7910142693f7d727df5d2ff2834b86d75e4f6
Yaxin <elfinhuangcat@gmail.com>
2015-10-16 15:19:41 -0400

    Updating graph_mod

--- HW2/HW2_code/graph_mod.py
+++ HW2/HW2_code/graph_mod.py
@@ -21,37 +21,40 @@ class Graph:
                 self.in_graph[arr[0]] = set(arr[1:]) # to avoid duplicate
             else:
                 self.in_graph[arr[0]] = self.in_graph[arr[0]] | set(arr[1:])
-            # Check if all pages are already in the dict.keys():
-            for page in arr[1:]:
-                if page not in self.in_graph.keys():
-                    self.in_graph[page] = set()
         input_graph.close()
         print "Finish in_graph construction."
-        
-        # graph nodes number:
-        self.node_num = len(self.in_graph.keys())
 
         # init sink node set, i.e. pages that have no out links
         # init the number of out-links (without duplicates) from page q
-        self.out_count = dict.fromkeys(self.in_graph.keys())
+        self.out_count = dict(zip(self.in_graph.keys(), 
+                                  [0] * len(self.in_graph)))
         self.__init_out_count()
         self.sink_node = set()
         self.__init_sink_nodes()
         self.source_node = set()
         self.__init_source_nodes()
+        # graph nodes number:
+        self.node_num = len(self.in_graph.keys())
         
     def __init_out_count(self):
         """
         Initialize out_count: key is the page P, value is 
         the number of pages that P points to.
         """
-        for key in self.out_count:
-            self.out_count[key] = set()
+        missing_pages = set()
         for key in self.in_graph:
             for page in self.in_graph[key]:
-                self.out_count[page].add(key)
-        for key in self.out_count: # turn it into counts
-            self.out_count[key] = len(self.out_count[key])
+                if page not in self.in_graph.keys():
+                    # record this page and add it to in_graph later
+                    missing_pages.add(page)
+                    self.out_count[page] = 0
+                self.out_count[page] += 1
+                #print("Finish " + page + " -> " + key)
+            #print("Finish counting: " + key)
+        # Add missing pages to in_graph
+        for page in missing_pages:
+            self.in_graph[page] = set()
+        print "Finish out_count construction"
 
     def __init_sink_nodes(self):
         for key in self.out_count:
@@ -91,6 +94,7 @@ class Graph:
 
 if __name__ == '__main__':
     start_time = time.time()
+    #graph = Graph("graphs/six_node_graph")
     graph = Graph("graphs/wt2g_inlinks.txt")
     graph.print_graph_info()
     end_time = time.time()
